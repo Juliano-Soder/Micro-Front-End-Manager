f5c026249047488fd7703fd932e3680abcda5f2a
JULIANO SODER <juliano.soder@viavarejo.com.br>
2025-06-25 09:56:02 -0300

    feature(index.html): Ajustado comportamento de mostrar os mfes e melhorado css

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: './OIP',
     win32metadata: {
       CompanyName: "Grupo Casas Bahia",
       FileDescription: "Gerenciador de Micro Front-Ends",
